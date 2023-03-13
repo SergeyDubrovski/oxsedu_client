4bc6935aea9302fff84d62fb9139885b5475f29c
SergeyDubrovski <sovelectron@gmail.com>
2023-03-13 07:16:49 +0300

    1st

---
+++ src/index.js
@@ -0,0 +1,46 @@
+import { Provider } from 'react-redux';
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+ 
+  <Provider>
+     <App />
+  </Provider>
+   
+ 
+);
+
+/*
+npx create-react-app .
+npm i redux react-redux
+
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
+
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8" />
+
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <title>oxsedu</title>
+  </head>
+  <body>
+    <div id="root"></div>
+  </body>
+</html>
+*/
+
